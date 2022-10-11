4c6125abe5e12842d4977e2b8b379c960eaabc04
Jerrylum <serverofjerry@gmail.com>
2022-10-11 17:54:20 +0800

    wip

--- workers/w1.py
+++ workers/w1.py
@@ -5,38 +5,20 @@ def run(worker: WorkerController):
     worker.init()
     worker.use_clock(frequency=100)
     sm = worker.use_serial_manager()
-    sm.whitelist.append(PortInfo(serial_number="5513132373735171A0B1", baudrate=115200))
-    sm.whitelist.append(PortInfo(serial_number="7513131383235170F071", baudrate=115200))
+    sm.whitelist.append(PortInfo(serial_number="55131323737351601271", baudrate=115200))
     sm.whitelist.append(PortInfo(serial_number="0001", baudrate=921600))
 
     gb.start_gateway(UDPBroadcast("255.255.255.255", 7986))
 
-    drive = gb.clone("drive")
-    catapult_trigger = 0
+    drive = [0, 0, 0, 0]
 
     while True:
 
-        drive[0] = isBtnPressing("kb:i")
-
-        elevator_delta = (isBtnPressing("kb:o") - isBtnPressing("kb:l"))
-        claw_x_delta = (isBtnPressing("kb:Right") - isBtnPressing("kb:Left"))
-        claw_y_delta = (isBtnPressing("kb:Up") - isBtnPressing("kb:Down"))
-
-        drive[1] = max(80, min(drive[1] + elevator_delta, 170))
-        drive[2] = max(0, min(drive[2] + claw_x_delta, 140))
-        drive[3] = max(0, min(drive[3] + claw_y_delta, 110))
-
-        if isBtnJustPressed("kb:g"):
-            catapult_trigger = 2
-        if isBtnJustReleased("kb:g"):
-            catapult_trigger = 1
-        if isBtnJustPressed("kb:f"):
-            if catapult_trigger != 1 and catapult_trigger != -1:
-                catapult_trigger = 1
-            catapult_trigger *= -1
-
-        gb.write("drive", list(drive))
-        gb.write("catapult_trigger", catapult_trigger)
+        drive[0] = 1200
+        drive[1] = 1200
+        drive[2] = 1200
+        drive[3] = 1200
+        gb.write("rs.o", list(drive))
 
         # print(drive)
         # print("feedback", gb.read("feedback"))
